0076eb65418e2cf3e1c245fbd5b1c2c2cb56cbbd
netmonk <netmonk@netmonk.org>
2017-06-19 23:22:45 +0200

    script.sh

--- script.sh
+++ script.sh
@@ -62,7 +62,7 @@ download_archive_and_extract() {
 build_kernel() {
 	cd "${work_dir}/kernel"
 	rm -rf ${installed_kerneL}
-	mkdir -p  "${installed_kerneL}"
+	mkdir -p  "${installed_kernel}"
 	#parsing config file for options 
 	cflags=$(get_value_from_conf CFLAGS)
 	job_factor=$(get_value_from_conf JOB_FACTOR)
