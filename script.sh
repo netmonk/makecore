d6767ffe93aad58f8704f408c21f4d9a04a1ae73
netmonk <netmonk@netmonk.org>
2017-06-19 17:11:31 +0200

    script.sh

--- script.sh
+++ script.sh
@@ -74,6 +74,6 @@ dl=$(download_archive_and_extract KERNEL_SOURCE_URL kernel)
 WriteInfo "Step 1 finished, kernel Sources retrieved and extracted" 
 
 WriteInfo "Step 2 let's build the kernel" 
-[[ ${dl} == 0  ]] && build_kernel || ExitScript  1 "unable to download and install kernel source tree, exiting!!!" 
+[[ ${dl} -eq  0  ]] && build_kernel || ExitScript  1 "unable to download and install kernel source tree, exiting!!!" 
 WriteInfo "Step 2 kernel build" 
 
